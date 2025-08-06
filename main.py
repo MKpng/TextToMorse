77b0e1b041a113edbcc352f52f7bebcc1d500172
Miguel Koche <miguel-koche1@outlook.com>
2025-08-06 17:37:30 -0300

    text convertion successfully

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 from morse import morse_dict
+
+user_input = str(input("Covert the follow sentence to MORSE: ")).upper()
+
+morsed = []
+for char in user_input:
+    if char in morse_dict:
+        morsed.append(morse_dict[char])
+
+print("".join(morsed).rstrip(" "))
