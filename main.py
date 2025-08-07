b67c9a18ea2bda58b7cf30aadbbc54bab52ce099
Miguel Koche <miguel-koche1@outlook.com>
2025-08-07 16:55:05 -0300

    text to sound

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import time
+
 from morse import morse_dict
+import winsound
 
 user_input = str(input("Covert the follow sentence to MORSE: ")).upper()
 
@@ -7,4 +10,14 @@ for char in user_input:
     if char in morse_dict:
         morsed.append(morse_dict[char])
 
-print("".join(morsed).rstrip(" "))
+morsed = "".join(morsed).rstrip(" ")
+
+for code in morsed:
+    if code == "○":
+        winsound.Beep(700, 150)
+    elif code == "▭":
+        winsound.Beep(700, 450)
+    elif code == "/":
+        time.sleep(1.05)
+print(morsed)
+
