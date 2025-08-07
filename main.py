4b05588192fd69261d3a858b09942c1cd6f30cc2
Miguel Koche <miguel-koche1@outlook.com>
2025-08-07 17:41:35 -0300

    first GUI version

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
-import time
-
 from morse import morse_dict
 import winsound
+import time
+from tkinter import *
+from tkinter import ttk
 
-user_input = str(input("Covert the follow sentence to MORSE: ")).upper()
+def get_morse():
+    value = morsed.get()
 
-morsed = []
-for char in user_input:
-    if char in morse_dict:
-        morsed.append(morse_dict[char])
+    morse = []
+    for char in value.upper():
+        if char in morse_dict:
+            morse.append(morse_dict[char])
 
-morsed = "".join(morsed).rstrip(" ")
+    morse = "".join(morse).rstrip(" ")
 
-for code in morsed:
-    if code == "○":
-        winsound.Beep(700, 150)
-    elif code == "▭":
-        winsound.Beep(700, 450)
-    elif code == "/":
-        time.sleep(1.05)
-print(morsed)
+    for code in morse:
+        if code == "○":
+            winsound.Beep(700, 150)
+        elif code == "▭":
+            winsound.Beep(700, 450)
+        elif code == "/":
+            time.sleep(1.05)
 
+root = Tk()
+frame = ttk.Frame(root)
+window_width = 600
+window_height = 600
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+x = (screen_width // 2) - (window_width // 2)
+y = (screen_height // 2) - (window_height // 2)
+root.geometry(f"{window_width}x{window_height}+{x}+{y}")
+frame.grid()
+
+ttk.Label(frame, text="Covert the follow sentence to MORSE").grid(columnspan=3, row=0)
+morsed = ttk.Entry()
+morsed.grid(column=2, row=1)
+click = ttk.Button(text="Convert", command=get_morse)
+click.grid(column=2, row=2)
+root.mainloop()
